1593fb272d2d5c319288c11409335b849e53fc4f
tulis <tanri.suryati@outlook.com>
2021-02-25 21:27:29 +1100

    Implement jambeo which will tell users what time it is currently now

--- src/JamBeo/Program.cs
+++ src/JamBeo/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Speech.Synthesis;
 
 namespace JamBeo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var now = DateTime.Now.ToString(format: "HH : mm tt");
+
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output.
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.SetOutputToDefaultAudioDevice();
+
+            // Speak a string.
+            Console.WriteLine(now);
+            synth.Speak(now);
         }
     }
 }
